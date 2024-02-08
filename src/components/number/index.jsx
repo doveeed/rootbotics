de808a4376f0abd1b30f54190521676c785b8c88
David Herbst <doveeedh@gmail.com>
2024-02-08 17:21:56 -0600

    Add files via upload

---
+++ src/components/number/index.jsx
@@ -0,0 +1,5 @@
+
+export default function Number({value}) {
+    
+    return (<b style={{backgroundColor: 'black', color: 'white', padding: '0px 8px', borderRadius: '4px', fontSize: '0.75rem'}}>+{value}</b>);
+}
