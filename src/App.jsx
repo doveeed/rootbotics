4005c3182846623b45e5dbe8a60df8134bb0daa1
David Herbst <doveeedh@gmail.com>
2024-02-09 11:24:24 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import DummyDuchy from "./components/bots/dummy-duchy";
 import CogwheelCult from "./components/bots/cogwheel-cult";
 import RivetfolkCompany from "./components/bots/rivetfolk-company";
 import Rootbotics from './assets/rootbotics.png';
+import Fall from './assets/fall.png';
+import Winter from './assets/winter.png';
+import Lake from './assets/lake.png';
+import Mountain from './assets/mountain.png';
+import DCMechanicalMarquise2point0 from "./components/bots/dc-mechanical-marquise-2point0";
 
 
 function App() {
@@ -39,7 +44,7 @@ function App() {
         <Menu bots={bots} onAddBot={(newBot) => {handleUpdateBots([...bots, {...newBot}])}} />
       </header>
       <main style={{flex: 1, display: 'flex', flexDirection: 'column'}}>
-        {isNoBots && (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1}}>Click + to add a bot.</div>)}
+        {isNoBots && (<div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', flex: 1, minHeight: 'calc(100vh - 80px)'}}>Click + to add a bot.</div>)}
       {bots.map((bot,index) => {
         const {faction, state, key} = bot;
         let Faction;
@@ -49,6 +54,9 @@ function App() {
             break;
           case 'contraption-conspiracy':
             Faction = ContraptionConspiracy;
+            break;
+          case 'dc-mechanical-marquise-2point0':
+            Faction = DCMechanicalMarquise2point0;
             break;
           case 'dummy-duchy':
             Faction = DummyDuchy;
@@ -81,6 +89,26 @@ function App() {
       <footer>
         {!isNoBots && (<button style={{width: '100%', backgroundColor: '#fcf8e8', border: 'none', cursor: 'pointer', padding: '24px 16px'}} onClick={() => {window.scrollTo(0,0)}}>Back to top</button>)}
       </footer>
+      {isNoBots && (
+          <div style={{padding: '0.5rem 1rem', display: 'flex',flexDirection: 'column', gap: '2rem', maxWidth: '740px', margin: '0 auto'}}>
+           <div>
+            <h2 style={{textAlign: 'center'}}>Fall</h2>
+            <img src={Fall} width="100%" alt="Fall map clearing priority setup"/>
+           </div>
+           <div>
+            <h2 style={{textAlign: 'center'}}>Winter</h2>
+            <img src={Winter} width="100%" alt="Winter map clearing priority setup"/>
+           </div>
+           <div>
+            <h2 style={{textAlign: 'center'}}>Lake</h2>
+            <img src={Lake} width="100%" alt="Lake map clearing priority setup"/>
+           </div>
+           <div>
+            <h2 style={{textAlign: 'center'}}>Mountain</h2>
+            <img src={Mountain} width="100%" alt="Mountain map clearing priority setup"/>
+           </div>
+          </div>
+        )}
     </div>
   );
 }
