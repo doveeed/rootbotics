8a198323931a23ecf338be3dde318b114d4637af
David Herbst <doveeedh@gmail.com>
2024-02-12 09:59:16 -0600

    Add files via upload

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,1 +1,10 @@
 
+export const CONSTANTS = {
+    eyrieNobilityText: <div style={{paddingLeft: '26px'}}><b>(Riverfolk)</b> If you cannot place a warrior, you fall into Turmoil.</div>,
+    eyrieWarTaxText:  <div style={{paddingLeft: '26px'}}><b>(War Tax)</b> If you destroy a token or building, the owner of the token or building loses 1 victory point.</div>,
+    humanRiverfolkLabelText: 'Check this box if there is a human Riverfolk player in the game',
+    riverfolkHandCardText: <div style={{paddingLeft: '26px'}}><b>(Riverfolk)</b> If the Riverfolk player has fewer victory points than you do, the order card has no craftable item, buy a card with a craftable item from the Riverfolk if available. If multiple cards exist, pick the one with the most VP for the item. If multiple, choose randomly.</div>,
+    riverfolkMercenariesBattleText: <div style={{paddingLeft: '26px'}}><b>(Riverfolk)</b> If the Riverfolk player has fewer victory points than you do, you have 2 or fewer warriors there, the Riverfolk is not the defender, and at least 1 Riverfolk warrior is present, then buy Mercenaries.</div>,
+    riverfolkMercenariesBuildText: <div style={{paddingLeft: '26px'}}><b>(Riverfolk)</b> If the Riverfolk player has fewer victory points than you do and buying Mercenaries would allow you to rule and build, buy Mercenaries.</div>,
+    riverfolkRiverBoatsText: <div style={{paddingLeft: '26px'}}><b>(Riverfolk)</b> If the Riverfolk player has fewer victory points than you do, treat rivers as paths. If at least one move would use the river, then buy River Boats.</div>,
+}
