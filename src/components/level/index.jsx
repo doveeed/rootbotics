dc086fbbd90d18feb15cb8f1ba4c96b30c4cb319
David Herbst <doveeedh@gmail.com>
2024-02-12 16:28:27 -0600

    Add files via upload

--- src/components/level/index.jsx
+++ src/components/level/index.jsx
@@ -7,21 +7,23 @@ export default function Level({faction,level, labels,onChangeLevel}) {
     
     return (
         <Card title="Difficulty Level">
-            <div>
-                <input type='radio' checked={level === 'beginner'} value="beginner" id={`${faction}-beginner`} onChange={onChange} />
-                <label htmlFor={`${faction}-beginner`}>Beginner = {labels['beginner']}</label>
-            </div>
-            <div>
-                <input type='radio' checked={level === 'expert'} value="expert" id={`${faction}-expert`} onChange={onChange} />
-                <label htmlFor={`${faction}-expert`}>Expert = {labels['expert']}</label>
-            </div>
-            <div>
-                <input type='radio' checked={level === 'master'} value="master" id={`${faction}-master`} onChange={onChange} />
-                <label htmlFor={`${faction}-master`}>Master = {labels['master']}</label>
-            </div>
-            <div>
-                <input type='radio' checked={level === 'boss'} value="boss" id={`${faction}-boss`} onChange={onChange} />
-                <label htmlFor={`${faction}-boss`}>Boss Mode (Co-op) = {labels['master']} Each round score <OneVP/> for every two human players (rounded up).</label>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
+                <label htmlFor={`${faction}-beginner`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
+                    <input type='radio' checked={level === 'beginner'} value="beginner" id={`${faction}-beginner`} onChange={onChange} />
+                    <div ><div><b>Beginner</b></div><div>{labels['beginner']}</div></div>
+                </label>
+                <label htmlFor={`${faction}-expert`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
+                    <input type='radio' checked={level === 'expert'} value="expert" id={`${faction}-expert`} onChange={onChange} />
+                    <div ><div><b>Expert</b></div><div>{labels['expert']}</div></div>
+                </label>
+                <label htmlFor={`${faction}-master`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
+                    <input type='radio' checked={level === 'master'} value="master" id={`${faction}-master`} onChange={onChange} />
+                    <div ><div><b>Master</b></div><div>{labels['master']}</div></div>
+                </label>
+                <label htmlFor={`${faction}-boss`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
+                    <input type='radio' checked={level === 'boss'} value="boss" id={`${faction}-boss`} onChange={onChange} />
+                    <div><div><b>Boss Mode (Co-op)</b></div><div>{labels['master']}<br/>Each round, score <OneVP/> for every two human players (rounded up).</div></div>
+                </label>
             </div>
     </Card>
 
