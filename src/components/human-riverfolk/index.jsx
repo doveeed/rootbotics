db671fba1f2ec2e7d24936cf1c6458d26da080ff
David Herbst <doveeedh@gmail.com>
2024-02-14 09:56:52 -0600

    Add files via upload

--- src/components/human-riverfolk/index.jsx
+++ src/components/human-riverfolk/index.jsx
@@ -1,1 +1,15 @@
+import { getFactionColor } from "../../utils";
+import Card from "../card";
 
+export default function HumanRiverfolk({isHumanRiverfolk, faction, onChange}) {
+    const accentColor = getFactionColor(faction);
+
+    return (
+        <Card title="Human Riverfolk">
+            <label htmlFor={`${faction}-human-riverfolk`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
+                <input id={`${faction}-human-riverfolk`} type="checkbox" onChange={() => onChange(!isHumanRiverfolk)} checked={isHumanRiverfolk} style={{accentColor}} />
+                <div>Check this box if there is a human Riverfolk player in the game.</div>
+            </label>
+        </Card>
+    )
+}
