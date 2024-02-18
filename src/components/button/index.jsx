7fe61b23c0745f2a7aee5e1b64496323d57e4347
David Herbst <doveeedh@gmail.com>
2024-02-17 21:59:16 -0600

    Add files via upload

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,1 +1,4 @@
 
+export default function Button({text, img, alt, onClick, style = {}, children, ...rest}) {
+    return (<button {...rest} style={{alignItems: 'center', background: 'none', border: `2px solid ${rest.disabled ? 'darkgray': 'black'}`, borderRadius: '4px', cursor: 'pointer', display: 'inline-flex', fontSize: '0.75rem', fontWeight: 'bold', lineHeight: '1rem', padding: '2px 4px',...style}} onClick={onClick}>{children}{img ? (<img style={{marginLeft: '0.25rem'}} src={img} alt={alt} width={16} height={16} />):''}</button>)
+}
