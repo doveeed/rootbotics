a4055cbbcb79b7f14ef114fd4232e120bf0c4775
David Herbst <doveeedh@gmail.com>
2024-02-19 14:39:14 -0600

    Add files via upload

--- src/components/bots/dc-electric-eyrie/buildings/index.jsx
+++ src/components/bots/dc-electric-eyrie/buildings/index.jsx
@@ -24,3 +24,20 @@ export default function Buildings({buildings = [], onUpdateBuildings}) {
         </div>
     );
 }
+
+export function BuildingsPreview({buildings = []}) {
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', margin: '1rem auto'}}>
+            <div style={{display: "flex", gap: '0.25rem', flexWrap: "wrap", width: '100%'}}>
+                {buildings.map(({id, isPlaced, points}) => (
+                    <div  key={id} style={{position: "relative", display: 'flex', flexGrow: '0', width: '2rem'}} >
+                        <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Roost} width="100%" alt="sawmill" />
+                        <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
+                            fontSize: '1rem',
+                            fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? 'blue' : 'transparent'}`,position: "absolute", top: 0,}}>{isPlaced && (<>+{points}</>)}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
