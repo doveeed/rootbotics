47e75571296c2978ff1683e3d18f7ef112636fde
David Herbst <doveeedh@gmail.com>
2024-02-19 14:38:47 -0600

    Add files via upload

--- src/components/bots/dc-automated-alliance/buildings/index.jsx
+++ src/components/bots/dc-automated-alliance/buildings/index.jsx
@@ -7,15 +7,10 @@ export default function Buildings({buildings, onUpdateBuildings}) {
     const {fox, rabbit, mouse} = buildings
 
     const handleClick = (type) => {
-
-        // const index = buildings.findIndex(({id: buildingId}) => buildingId === id);
-        // const building = buildings[index];
-
-        // const before = buildings.slice(0,index);
-        // const after = buildings.slice(index + 1);
         onUpdateBuildings({...buildings, [type]: {...buildings[type], isPlaced: !buildings[type].isPlaced}})
 
     }
+
     return (
         <div>
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
@@ -45,6 +40,55 @@ export default function Buildings({buildings, onUpdateBuildings}) {
                         }}/>
                     </div>
                     <div  style={{position: 'relative', cursor: 'pointer', display: 'flex', flex: '1'}} onClick={() => {handleClick(mouse.suit)}}>
+                        <img src={MouseBase} style={{opacity: mouse.isPlaced ? '50%': '100%'}} width="100%" alt='mouse base'/>
+                        <div style={{
+                            width: '100%',
+                            height: '100%',
+                            border: `4px solid ${mouse.isPlaced ? '#6db456': 'transparent'}`,
+                            borderRadius: '15%',
+                            position: 'absolute',
+                            boxSizing: 'border-box',
+                            top: 0,
+                        }}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export function BuildingsPreview({buildings}) {
+    const {fox, rabbit, mouse} = buildings
+    
+    return (
+        <div>
+            <div style={{display: 'flex', alignItems: 'center'}}>
+                <div style={{display: 'flex', gap: '0.25rem', margin: '1rem 0'}}>
+                    <div  style={{position: 'relative', cursor: 'pointer', display: 'flex', flex: '1', minWidth: '2rem', maxWidth: '2rem',}} >
+                        <img src={FoxBase} style={{opacity: fox.isPlaced ? '50%': '100%'}} width="100%" alt='fox base'/>
+                        <div style={{
+                            width: '100%',
+                            height: '100%',
+                            border: `4px solid ${fox.isPlaced ? '#6db456': 'transparent'}`,
+                            borderRadius: '15%',
+                            position: 'absolute',
+                            boxSizing: 'border-box',
+                            top: 0,
+                        }}/>
+                    </div>
+                    <div  style={{position: 'relative', cursor: 'pointer', display: 'flex', flex: '1', minWidth: '2rem', maxWidth: '2rem',}} >
+                        <img src={RabbitBase} style={{opacity: rabbit.isPlaced ? '50%': '100%'}} width="100%" alt='rabbit base'/>
+                        <div style={{
+                            width: '100%',
+                            height: '100%',
+                            border: `4px solid ${rabbit.isPlaced ? '#6db456': 'transparent'}`,
+                            borderRadius: '15%',
+                            position: 'absolute',
+                            boxSizing: 'border-box',
+                            top: 0,
+                        }}/>
+                    </div>
+                    <div  style={{position: 'relative', cursor: 'pointer', display: 'flex', flexGrow: 0, width: '2rem'}} >
                         <img src={MouseBase} style={{opacity: mouse.isPlaced ? '50%': '100%'}} width="100%" alt='mouse base'/>
                         <div style={{
                             width: '100%',
