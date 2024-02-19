d55c3b05909eaa9493c2544090b160c1bbf71f5c
David Herbst <doveeedh@gmail.com>
2024-02-19 14:40:14 -0600

    Add files via upload

--- src/components/bots/dummy-duchy/buildings/index.jsx
+++ src/components/bots/dummy-duchy/buildings/index.jsx
@@ -3,6 +3,7 @@ import Citadel2 from '../../../../assets/citadel2.png';
 import CitadelBuilding from '../../../../assets/citadel-building.png';
 import Market from '../../../../assets/market.png'
 import MarketBuilding from '../../../../assets/market-building.png';
+
 export default function Buildings({buildings, onUpdateBuildings}) {
 
     const handleClick = (id) => {
@@ -53,6 +54,45 @@ export default function Buildings({buildings, onUpdateBuildings}) {
                         </div>
                     ))}
                 </div>
+            </div>
+        </div>
+    );
+}
+
+export function BuildingsPreview({buildings}) {
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', gap: '0.25rem', margin: '1rem auto'}}>
+            <div style={{display: 'flex', gap: '0.25rem',}}>
+                    {buildings.filter(({type}) => type === 'citadel').map(({id, isPlaced}, index) => (
+                        <div key={id} style={{position: 'relative', display: 'flex', flexGrow: '0', width: '2rem'}}>
+                        <img key={id} src={isPlaced ? index < 2 ? Citadel1: Citadel2 : CitadelBuilding} width="100%" alt='citadel'/>
+                        <div style={{
+                                width: '100%',
+                                height: '100%',
+                                border: `2px solid ${isPlaced ? '#d2a88d': 'transparent'}`,
+                                borderRadius: '15%',
+                                position: 'absolute',
+                                boxSizing: 'border-box',
+                                top: 0,
+                            }}></div>
+                        </div>
+                    ))}
+            </div>
+            <div style={{display: 'flex', gap: '0.25rem'}}>
+                {buildings.filter(({type}) => type === 'market').map(({id, isPlaced}) => (
+                    <div key={id} style={{position: 'relative', display: 'flex', flexGrow: '0', width: '2rem'}}>
+                    <img key={id} src={isPlaced ? Market : MarketBuilding} width="100%" title="market" alt='market'/>
+                    <div style={{
+                            width: '100%',
+                            height: '100%',
+                            position: 'absolute',
+                            border: `2px solid ${isPlaced ? '#d2a88d': 'transparent'}`,
+                            borderRadius: '15%',
+                            top: 0,
+                            boxSizing: 'border-box',
+                        }}></div>
+                    </div>
+                ))}
             </div>
         </div>
     );
