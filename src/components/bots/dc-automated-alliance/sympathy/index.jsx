b4bc45d9f958addaf0eaeb2777e6d1ee7ebabcb2
David Herbst <doveeedh@gmail.com>
2024-02-20 14:05:04 -0600

    Add files via upload

--- src/components/bots/dc-automated-alliance/sympathy/index.jsx
+++ src/components/bots/dc-automated-alliance/sympathy/index.jsx
@@ -30,7 +30,7 @@ export function SympathyPreview({sympathy = []}) {
                     <div key={id} style={{position: "relative", display: 'flex', flexGrow: 0, width: '2rem'}} >
                         <img src={SympathyToken} style={{opacity: isPlaced ? '50%' : '100%'}} width="100%" alt="fox trade post"/>
                         <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
-                            fontSize: '1.25rem',
+                            fontSize: '1.5rem',
                             fontWeight: 'bold', borderRadius: '50%', border: `2px solid ${isPlaced ? '#6db456' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                     </div>
                 ))}
