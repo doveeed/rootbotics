4d74a183373af084cb011bbf4f140b09295d6499
David Herbst <doveeedh@gmail.com>
2024-02-22 09:13:13 -0600

    Add files via upload

--- src/components/bots/dc-mechanical-marquise-2point0/buildings/index.jsx
+++ src/components/bots/dc-mechanical-marquise-2point0/buildings/index.jsx
@@ -2,8 +2,10 @@ import Sawmill from '../../../../assets/sawmill.png';
 import Workshop from '../../../../assets/workshop.png';
 import Recruiter from '../../../../assets/recruiter.png';
 import Card from '../../../card';
+import { useSettings } from '../../../../hooks/use-settings';
 
 export default function Buildings({buildings = {}, onUpdateBuildings}) {
+    const {factionColor } = useSettings();
     const {sawmill = [], workshop = [], recruiter = []} = buildings;
 
     const handleClick = ({index, type}) => {
@@ -24,7 +26,7 @@ export default function Buildings({buildings = {}, onUpdateBuildings}) {
                             <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Sawmill} width="100%" alt="sawmill" />
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                                 fontSize: '1.5rem',
-                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                         </div>
                     ))}
                     </div>
@@ -37,7 +39,7 @@ export default function Buildings({buildings = {}, onUpdateBuildings}) {
                             <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Workshop} width="100%" alt="workshop" />
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                                 fontSize: '1.5rem',
-                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                         </div>
                     ))}
                     </div>
@@ -50,7 +52,7 @@ export default function Buildings({buildings = {}, onUpdateBuildings}) {
                             <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Recruiter} width="100%" alt="recruiter" />
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                                 fontSize: '1.5rem',
-                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                         </div>
                     ))}
                     </div>
@@ -61,6 +63,7 @@ export default function Buildings({buildings = {}, onUpdateBuildings}) {
 }
 
 export function BuildingsPreview({buildings = {}, orderedSuit, birdBuild}) {
+    const {factionColor } = useSettings();
     const {sawmill = [], workshop = [], recruiter = []} = buildings;
 
     return (
@@ -71,7 +74,7 @@ export function BuildingsPreview({buildings = {}, orderedSuit, birdBuild}) {
                         <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Sawmill} width="100%" alt="sawmill" />
                         <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                             fontSize: '1.5rem',
-                            fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                            fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                     </div>
                 ))}
                 </div>)}
@@ -81,7 +84,7 @@ export function BuildingsPreview({buildings = {}, orderedSuit, birdBuild}) {
                     <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Workshop} width="100%" alt="workshop" />
                     <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                         fontSize: '1.5rem',
-                        fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                        fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                 </div>
             ))}
             </div>)}
@@ -91,7 +94,7 @@ export function BuildingsPreview({buildings = {}, orderedSuit, birdBuild}) {
                    <img style={{opacity: isPlaced ? '50%' : '100%', borderRadius: '15%'}} src={Recruiter} width="100%" alt="recruiter" />
                    <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                        fontSize: '1.5rem',
-                       fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? 'orange' : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
+                       fontWeight: 'bold', borderRadius: '15%', border: `2px solid ${isPlaced ? factionColor : 'transparent'}`,position: "absolute", top: 0}}>{isPlaced && (<>+{points}</>)}</div>
                     </div>
                 ))}
             </div>)}
