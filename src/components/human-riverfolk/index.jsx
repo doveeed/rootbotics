4770f719a2663ff872f5cc28ad062c2d8a561a84
David Herbst <doveeedh@gmail.com>
2024-02-22 09:11:24 -0600

    Add files via upload

--- src/components/human-riverfolk/index.jsx
+++ src/components/human-riverfolk/index.jsx
@@ -1,13 +1,13 @@
-import { getFactionColor } from "../../utils";
+import { useSettings } from "../../hooks/use-settings";
 import Card from "../card";
 
-export default function HumanRiverfolk({isHumanRiverfolk, faction, onChange}) {
-    const accentColor = getFactionColor(faction);
+export default function HumanRiverfolk({isHumanRiverfolk,  onChange}) {
+    const {faction, factionColor} = useSettings();
 
     return (
         <Card title="Human Riverfolk">
             <label htmlFor={`${faction}-human-riverfolk`} style={{display: 'flex', gap: '1rem', alignItems: 'center', cursor: 'pointer'}}>
-                <input id={`${faction}-human-riverfolk`} type="checkbox" onChange={() => onChange(!isHumanRiverfolk)} checked={isHumanRiverfolk} style={{accentColor}} />
+                <input id={`${faction}-human-riverfolk`} type="checkbox" onChange={() => onChange(!isHumanRiverfolk)} checked={isHumanRiverfolk} style={{accentColor: factionColor}} />
                 <div>Check this box if there is a human Riverfolk player in the game.</div>
             </label>
         </Card>
