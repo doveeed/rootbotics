b28348c52f7bf4746a6eb74121f8ead2d900a2d2
David Herbst <doveeedh@gmail.com>
2024-02-22 09:10:51 -0600

    Add files via upload

--- src/hooks/use-settings/index.jsx
+++ src/hooks/use-settings/index.jsx
@@ -1,1 +1,23 @@
 
+import React, { createContext, useContext } from 'react';
+
+
+const SettingsContext = createContext({
+  enableDynamicTables: false,
+  enableCCPSupport: false
+});
+
+export function SettingsContextProvider({
+  children,
+  ...props
+}) {
+  return (
+    <SettingsContext.Provider value={{...props}}>
+      {children}
+    </SettingsContext.Provider>
+  );
+}
+
+export function useSettings() {
+  return useContext(SettingsContext);
+}
