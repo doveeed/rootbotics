000c311eb4337ef2709b580231b700093f09a463
David Herbst <doveeedh@gmail.com>
2024-02-28 10:23:25 -0600

    Add files via upload

--- src/components/bots/dc-vagabot/items/index.jsx
+++ src/components/bots/dc-vagabot/items/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Item from '../../../../assets/item.png';
 import { useSettings } from '../../../../hooks/use-settings';
 import { getRandomKey } from '../../../../utils';
@@ -8,41 +8,32 @@ import Card from '../../../card';
 export default function Items({items = [], onUpdateItems = () => {}}) {
     const {factionColor } = useSettings();
     const [isLongPressed, setIsLongPressed] = useState(false);
-    const [clickedItemIndex, setClickedItemIndex] = useState(-1);
+    const ref = useRef(undefined);
 
-    useEffect(() => {
-        if (clickedItemIndex === -1) {
-            return;
-        }
-        const timeoutId = setTimeout(() => {
-
-            const before = items.slice(0,clickedItemIndex);
-            const after = items.slice(clickedItemIndex + 1);
-                setIsLongPressed(true);
-                onUpdateItems([...before, {...items[clickedItemIndex], isDamaged: !items[clickedItemIndex].isDamaged}, ...after])
-        }, 500);
-
-            return () => {
-                clearTimeout(timeoutId);
-            };
-    }, [isLongPressed, clickedItemIndex, onUpdateItems, items]);
-
-    const handleMouseDown = (index) => {
-        setClickedItemIndex(index);
+    const handleMouseDown = useCallback((index) => {
         setIsLongPressed(false);
-    };
+
+        ref.current = setTimeout(() => {
+            const before = items.slice(0,index);
+            const after = items.slice(index + 1);
+                setIsLongPressed(true);
+                onUpdateItems([...before, {...items[index], isDamaged: !items[index].isDamaged}, ...after])
+        }, 600);
+    }, [ref, items, onUpdateItems]);
     
-    const handleMouseUp = (index) => {
+    const handleMouseUp = useCallback((index) => {
         const before = items.slice(0,index);
         const after = items.slice(index + 1);
-        setClickedItemIndex(-1);
+        if (ref.current) {
+            clearTimeout(ref.current);
+            ref.current = undefined;
+        }
         if (isLongPressed) {
-            
             return;
         }
 
         onUpdateItems([...before, {...items[index], isExausted: !items[index].isExausted}, ...after]);
-    };
+    }, [isLongPressed, items, onUpdateItems]);
 
 
 
@@ -52,13 +43,14 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
             <div style={{display: 'flex', flexDirection: 'column', marginBottom: '1rem'}}>
                 <div style={{display: "grid", gap: '0.5rem',  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', flexWrap: "wrap", width: '100%'}}>
                     {items.map(({key, isExausted, isDamaged}, index) => (
-                        <div onTouchStart={() => handleMouseDown(index)} onMouseDown={() => handleMouseDown(index)} onMouseUp={() => handleMouseUp(index)} onTouchEnd={() => handleMouseUp(index)}  key={key} style={{position: "relative", cursor: "pointer", display: 'flex', flex: 1,}} >
+                        <div onTouchStart={() => handleMouseDown(index)} onMouseDown={() => handleMouseDown(index)} onMouseUp={() => handleMouseUp(index)} onTouchEnd={() => handleMouseUp(index)}  key={key} style={{position: "relative", userSelect: 'none', cursor: "pointer", display: 'flex', flex: 1,}} >
                             <img style={{opacity: '100%', borderRadius: '15%'}} src={Item} width="100%" alt="sawmill" />
                             {isExausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                                 fontSize: '1.25rem',
                                 wordBreak: 'break-all',
                                 textAlign: 'center',
+                                userSelect: 'none',
                                 fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${factionColor}`,position: "absolute", top: 0,}}>{isDamaged && (<>Damaged</>)}</div>
                         </div>
                     ))}
