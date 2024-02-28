55d6850225d75e8650f126b0499d247a366823f5
David Herbst <doveeedh@gmail.com>
2024-02-28 09:53:12 -0600

    Add files via upload

--- src/components/bots/dc-vagabot/characters/index.jsx
+++ src/components/bots/dc-vagabot/characters/index.jsx
@@ -1,1 +1,20 @@
+import { useSettings } from "../../../../hooks/use-settings";
+import Card from "../../../card";
 
+export default function Characters({characters, selectedCharacter, onChangeCharacter}) {
+    const {factionColor} = useSettings();
+
+    return (
+        <Card title="Characters">
+            {Object.values(characters).map(({id, name, special}) => (
+                 <label htmlFor={id} style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem', gap: '1rem', cursor: 'pointer'}} >
+                    <input id={id} type="checkbox" style={{accentColor: factionColor}} checked={selectedCharacter === id}  onChange={() => onChangeCharacter(id)}/>
+                    <div>
+                        <div><b>{name}</b></div>
+                        <div>{special}</div>
+                    </div>
+                </label>
+            ))}
+        </Card>
+    );
+}
