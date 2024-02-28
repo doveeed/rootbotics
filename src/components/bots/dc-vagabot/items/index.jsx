84b0425c832a827c01eee004755a2f5807560db8
David Herbst <doveeedh@gmail.com>
2024-02-28 10:40:53 -0600

    Add files via upload

--- src/components/bots/dc-vagabot/items/index.jsx
+++ src/components/bots/dc-vagabot/items/index.jsx
@@ -21,20 +21,22 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
         }, 600);
     }, [ref, items, onUpdateItems]);
     
-    const handleMouseUp = useCallback((index) => {
-        console.log('DAH: click', isLongPressed)
+    const handleMouseUp = useCallback(() => {
         if (ref.current) {
             clearTimeout(ref.current);
             ref.current = undefined;
         }
+    }, []);
+
+    const handleClick = useCallback((index) => {
         if (isLongPressed) {
             return;
         }
-
+        console.log('DAH: click', index);
         const before = items.slice(0,index);
         const after = items.slice(index + 1);
         onUpdateItems([...before, {...items[index], isExausted: !items[index].isExausted}, ...after]);
-    }, [isLongPressed, items, onUpdateItems]);
+    },  [isLongPressed, onUpdateItems, items]);
 
 
 
@@ -44,7 +46,7 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
             <div style={{display: 'flex', flexDirection: 'column', marginBottom: '1rem'}}>
                 <div style={{display: "grid", gap: '0.5rem',  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', flexWrap: "wrap", width: '100%'}}>
                     {items.map(({key, isExausted, isDamaged}, index) => (
-                        <div onTouchStart={() => handleMouseDown(index)} onMouseDown={() => handleMouseDown(index)} onClick={() => handleMouseUp(index)} key={key} style={{position: "relative", userSelect: 'none', cursor: "pointer", display: 'flex', flex: 1,}} >
+                        <div onTouchStart={() => handleMouseDown(index)} onTouchEnd={() => handleMouseUp()} onMouseDown={() => handleMouseDown(index)} onMouseUp={() => handleMouseUp()} onClick={() => handleClick(index)} key={key} style={{position: "relative", userSelect: 'none', cursor: "pointer", display: 'flex', flex: 1,}} >
                             <img style={{opacity: '100%', borderRadius: '15%'}} src={Item} width="100%" alt="sawmill" />
                             {isExausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
