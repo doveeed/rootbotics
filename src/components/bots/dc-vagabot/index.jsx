40a58ad1df3570d2549573adcaba6f9b11811175
David Herbst <doveeedh@gmail.com>
2024-03-04 10:04:26 -0600

    Add files via upload

--- src/components/bots/dc-vagabot/index.jsx
+++ src/components/bots/dc-vagabot/index.jsx
@@ -9,15 +9,15 @@ import Trait from '../../trait';
 import OneVP from "../../one-vp";
 import { CONSTANTS } from "../../../utils";
 import HumanRiverfolk from "../../human-riverfolk";
-import Items, { ItemsPreview } from "./items";
+import Items, { ItemsPreview, isBattleTrack } from "./items";
 import VP2 from '../../../assets/vp2.png';
 import Characters from "./characters";
 
 const characters = {
     thief: {id: 'thief', name: 'Thief', special:  <>Take a random card from the enemy in your clearing with the most victory points. On a tie, take it from such an enemy with the most pieces there.</>},
-    tinker: {id: 'tinker', name: 'Tinker', special:  <>Search the discard pile for the topmost card with an available item and craft it. <i>(Do not score a point)</i></>},
-    ranger: {id: 'ranger', name: 'Ranger', special:  <>If you have three or more damaged items, slip into a random adjacent forest.</>},
-    vagrant: {id: 'vagrant', name: 'Vagrant', special:  <>Initiate a battle in your clearing. You choose the attacker and then the defender <i>(using setup priority)</i>, and you remove pieces for each.</>},
+    tinker: {id: 'tinker', name: 'Tinker', special:  <>Search the discard pile for the topmost card with an available item and craft it, gaining no points.</>},
+    ranger: {id: 'ranger', name: 'Ranger', special:  <>If you have any damaged items, repair 1 damaged item, unexhausted before exhausted.</>},
+    vagrant: {id: 'vagrant', name: 'Vagrant', special:  <>Initiate a battle in your clearing. Select the player with the most pieces as Attacker, and the player with the second-most pieces as Defender. Only select yourself if there are no other options, and always as Attacker. When removing pieces, prioritise buildings over tokens (choose randomly between multiple types of buildings or tokens).</>},
     scoundrel: {id: 'scoundrel', name: 'Scoundrel', special:  <>If your clearing has 3 or more enemy pieces, including 1 building or token, remove all enemy pieces there. Place any one of your items in your clearing, covering a building slot. Buildings cannot be placed in this slot. Score <OneVP />.</>},
     arbiter: {id: 'arbiter', name: 'Arbiter', special:  <>Before rolling in battle, the defender may enlist the Arbiter if the pawn is in the battle clearing. <i>(If other bots are in play, they will interact with the Arbiter, whether played by a human or bot, in the same way.)</i> The Arbiter scores one victory point and adds the number of items on his Battle Track to the defender's maximum rolled hits. Defending bots will enlist the Arbiter if they meet all three of the following conditions:
     <br/><br/>I. Their maximum rolled hits is less than three.
@@ -33,40 +33,47 @@ export default function DCVagabot({ state = {}, isRivetfolkPlaying, onDelete = (
         bird: ['explore', 'quest', 'battle']
     }
 
+    const totalDamagedItems = items.filter(({isDamaged}, index) => !isBattleTrack(index) && isDamaged).length;
+    const totalUndamagedItems = items.filter(({isDamaged}, index) => !(isBattleTrack(index) || isDamaged)).length;
+
     const maxHits = items.length >= 12 ? <>3, <b>deal an extra hit</b></> : items.length >= 9 ? 3 : items.length >= 6 ? 2 : 1;
 
     const steps = {
-        aid: <Step title="Aid." description={<>Move to the nearest clearing containing pieces of a player with any items if possible. Then exhaust one item to take one of their items and place it in your satchel. Score <OneVP/> and then they draw one card.</>} substeps={<Steps type="I" steps={[<Step title={<i>Player tie:</i>} description={<i>If multiple players with items have pieces in equal distance, select the player with the lower score.</i>}/>]}/>}/>,
-        battle: <Step title="Battle." description={<>Move to the nearest clearing with any peices of the enemy with the most points, then exhaust one item to battle that player. Your maximum Rolled Hits is {maxHits}. Score <OneVP/> per enemy warrior removed. Repeat this action, exhausting two items per extra battle, as many times as possible.</>} substeps={<Steps type="I" steps={[<Step title={<i>Clearing Tie:</i>} description={<i>Move to the clearing where the target player has the most tokens and buildings, then fewest warriors.</i>} />]} />}/>,
-        explore: <Step title="Explore." description="Move to the nearest ruin, then exhoust one item to take an item from it."/>,
-        quest: <Step title="Quest." description={<>Move to the nearest clearing matching your quest. Then exhaust any two items to discard your quest and score  <img src={VP2} alt="two victory points" width={24} style={{marginBottom: '-4px'}}/>. Then, replace the quest.</>} />,
-        special: <Step title="Special." description={<>Exhaust one item to take the following action: {characters[character].special} Skip this action if it would have no effect.</>} />,
+        aid: <Step title="Aid." description={<>Move to the nearest clearing containing pieces of a player with any items if possible. Then exhaust 1 item to take 1 of their items and place it in your satchel. Score <OneVP/> and then they draw 1 card.</>} substeps={<Steps type="I" steps={[<Step title={<i>Player tie:</i>} description={<i>If multiple players with items have pieces in equal distance, select the player with the lower score.</i>}/>]}/>}/>,
+        battle: <Step title="Battle." description={<>Move to the nearest clearing with any peices of the enemy with the most points, then exhaust 1 item to battle that player. Your maximum Rolled Hits is {maxHits}. Score <OneVP/> per enemy warrior removed. Repeat this action, exhausting 2 items per extra battle, as many times as possible.</>} substeps={<Steps type="I" steps={[<Step title={<i>Clearing Tie:</i>} description={<i>Move to the clearing where the target player has the most tokens and buildings, then fewest warriors.</i>} />]} />}/>,
+        explore: <Step title="Explore." description="Move to the nearest ruin, then exhoust 1 item to take an item from it."/>,
+        quest: <Step title="Quest." description={<>Move to the nearest clearing matching your quest. Then exhaust any 2 items to discard your quest and score  <img src={VP2} alt="two victory points" width={24} style={{marginBottom: '-4px'}}/>. Then, replace the quest.</>} />,
+        special: <Step title="Special." description={<>Exhaust 1 item to take the following action: {characters[character].special} Skip this action if it would have no effect.</>} />,
     }
 
     const isBossMode = level === 'boss';
     // const isIronWill = traits.some(({id, isEnabled}) => id === 'iron-will' && isEnabled);
     // const isBlitz = traits.some(({id, isEnabled}) => id === 'blitz' && isEnabled);
     const levelToRefresh = {
-        'beginner': '3',
-        'expert': '4',
-        'master': '5',
-        'boss': '5',
+        'beginner': 3,
+        'expert': 4,
+        'master': 5,
+        'boss': 5,
     }
     const canBuyServices = isRivetfolkPlaying || isHumanRiverfolk;
 
     const birdsongSteps = [
         <Step title="Reveal" description="the top card of the deck as order card."/>,
         <Step title="Craft" description={<>order card for <OneVP/> if it shows an available item.{canBuyServices ? CONSTANTS.riverfolkHandCardText:''}</>} />,
-        <Step title="Slip." description="If you have two or fewer undamaged items, move to a random adjacent forest, then go to Evening." />,
     ]
+
+    if (totalUndamagedItems <= 2) {
+        birdsongSteps.push(
+            <Step title="Slip." description="Move to a random adjacent forest, then go to Evening." />);
+    }
 
     
     const daylightSteps = suitToSteps[orderedSuit].filter((step) => !(character === 'arbiter' && step === 'special')).map(step => steps[step]);
     
 
     const eveningSteps = [
-        <Step title="Refresh" description={<>{levelToRefresh[level]} items. If you have no damaged items, refresh 2 more.</>} />,
-        <Step title="Repair." description="If you are in a forest, repair all items. If not, repair one item. Repair unexhausted items before exhausted items." />,
+        <Step title="Refresh" description={<>{levelToRefresh[level] + (totalDamagedItems === 0 ? 2: 0)} items.</>} />,
+        <Step title="Repair." description="If you are in a forest, repair all items. If not, repair 1 item. Repair unexhausted items before exhausted items." />,
         <Step title="Discard" description="the order card."/>
     ];
 
@@ -97,8 +104,8 @@ export default function DCVagabot({ state = {}, isRivetfolkPlaying, onDelete = (
                                 }
                             />
                         </Card >
-                        <Characters characters={characters} selectedCharacter={character} onChangeCharacter={(id) => updateState({...state, character: id})}/>
-                        <Level  level={level} labels={{'beginner': <>Whenever you <b>Recruit</b>, place <b>3 warriors</b>.</>, 'expert': <>Whenever you <b>Recruit</b>, place <b>4 warriors</b>.</>, 'master': <>Whenever you <b>Recruit</b>, place <b>5 warriors</b>.</>}} onChangeLevel={(newLevel) => updateState({...state, level: newLevel})} />
+                        <Characters characters={characters} selectedCharacter={character} onChangeCharacter={(updates) => updateState({...state, ...updates})}/>
+                        <Level  level={level} labels={{'beginner': <>Refresh <b>3 items</b> in Evening.</>, 'expert':  <>Refresh <b>4 items</b> in Evening.</>, 'master':  <>Refresh <b>5 items</b> in Evening.</>}} onChangeLevel={(newLevel) => updateState({...state, level: newLevel})} />
                         {!isRivetfolkPlaying && (<HumanRiverfolk  onChange={(newIsHumanRiverfolk) => updateState({...state, isHumanRiverfolk: newIsHumanRiverfolk})}/>)}
                     </>
                 )}
@@ -127,7 +134,7 @@ export default function DCVagabot({ state = {}, isRivetfolkPlaying, onDelete = (
                         </Card>
                         <Card title="Daylight" headerBackgroundColor="#6db6dc" headerColor="white">
                         <ItemsPreview items={items} />
-                            <div style={{marginBottom: '1rem'}}>Take the following ordered actions. when ordered to move, exhaust one item per move.</div>
+                            <div style={{marginBottom: '1rem'}}>Take the following ordered actions. when ordered to move, exhaust 1 item per move.</div>
                             <Step description="" />
                             <Steps 
                                 type="1"
