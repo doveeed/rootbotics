2e65e7d5cf1ca94b7012edbe944162d109f69983
David Herbst <doveeedh@gmail.com>
2024-03-07 10:17:01 -0600

    Add files via upload

--- src/components/bots/dc-vagabot/items/index.jsx
+++ src/components/bots/dc-vagabot/items/index.jsx
@@ -42,7 +42,7 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
 
         const before = items.slice(0,index);
         const after = items.slice(index + 1);
-        onUpdateItems([...before, {...items[index], isExausted: !items[index].isExausted}, ...after]);
+        onUpdateItems([...before, {...items[index], isExhausted: !items[index].isExhausted}, ...after]);
     },  [isLongPressed, onUpdateItems, items]);
 
     return (
@@ -50,17 +50,17 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
         <Card title="Battle Track">
             <div style={{display: 'flex', flexDirection: 'column', marginBottom: '1rem'}}>
                 <div style={{display: "grid", gap: '0.5rem',  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', flexWrap: "wrap", width: '100%'}}>
-                    {items.map(({key, isExausted, isDamaged}, index) => isBattleTrack(index) ? (
+                    {items.map(({key, isExhausted, isDamaged}, index) => isBattleTrack(index) ? (
                         <div key={key} style={{position: "relative", userSelect: 'none', display: 'flex', flex: 1,}} >
                             <img style={{opacity: '100%', borderRadius: '15%'}} src={Item} width="100%" alt="sawmill" />
-                            {isExausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
+                            {isExhausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
-                                fontSize: '1.25rem',
+                                fontSize: '1rem',
                                 wordBreak: 'break-all',
                                 textAlign: 'center',
                                 userSelect: 'none',
                                 overflow: 'hidden',
-                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${factionColor}`,position: "absolute", top: 0,}}><div style={{transform:'rotate(-45deg)', lineHeight: '1rem'}}>{isDamaged && (<>Damaged</>)}{isBattleTrack(index) && (<>Battle Track</>)}</div></div>
+                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${factionColor}`,position: "absolute", top: 0,}}><div style={{transform:'rotate(-45deg)', lineHeight: '1rem'}}>{isDamaged && (<>Damaged</>)}{isBattleTrack(index) && (<>Battle</>)}</div></div>
                         </div>
                     ): null)}
                 </div>
@@ -71,24 +71,24 @@ export default function Items({items = [], onUpdateItems = () => {}}) {
         <Card title="Satchel">
             <div style={{display: 'flex', flexDirection: 'column', marginBottom: '1rem'}}>
                 <div style={{display: "grid", gap: '0.5rem',  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr', flexWrap: "wrap", width: '100%'}}>
-                    {items.map(({key, isExausted, isDamaged}, index) => isBattleTrack(index) ? null :(
+                    {items.map(({key, isExhausted, isDamaged}, index) => isBattleTrack(index) ? null :(
                         <div onTouchStart={() => handleMouseDown(index)} onTouchEnd={() => handleMouseUp()} onMouseDown={() => handleMouseDown(index)} onMouseUp={() => handleMouseUp()} onClick={() => handleClick(index)} key={key} style={{position: "relative", userSelect: 'none', cursor: "pointer", display: 'flex', flex: 1,}} >
                             <img style={{opacity: '100%', borderRadius: '15%'}} src={Item} width="100%" alt="sawmill" />
-                            {isExausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
+                            {isExhausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
                             <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
-                                fontSize: '1.25rem',
+                                fontSize: '1rem',
                                 wordBreak: 'break-all',
                                 textAlign: 'center',
                                 userSelect: 'none',
                                 overflow: 'hidden',
-                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${factionColor}`,position: "absolute", top: 0,}}><div style={{transform:'rotate(-45deg)', lineHeight: '1rem'}}>{isDamaged && (<>Damaged</>)}{isBattleTrack(index) && (<>Battle Track</>)}</div></div>
+                                fontWeight: 'bold', borderRadius: '15%', border: `4px solid ${factionColor}`,position: "absolute", top: 0,}}><div style={{transform:'rotate(-45deg)', lineHeight: '1rem'}}>{isDamaged && (<>Damaged</>)}{isBattleTrack(index) && (<>Battle</>)}</div></div>
                         </div>
                     ))}
                 </div>
             </div>
             <div style={{display: 'flex', flexDirection: 'row', gap: '4px'}}>
                 <Button onClick={() => {
-                    onUpdateItems([...items, {key: getRandomKey(), isExausted: false, isDamaged: false}])
+                    onUpdateItems([...items, {key: getRandomKey(), isExhausted: false, isDamaged: false}])
                 }}>Add item</Button>
                 <Button onClick={() => {
                     onUpdateItems(items.slice(0, -1))
@@ -105,10 +105,10 @@ export function ItemsPreview({items = []}) {
     return (
         <div style={{display: 'flex', flexDirection: 'column', marginBottom: '1rem'}}>
             <div style={{display: "flex", gap: '0.25rem', flexWrap: "wrap", width: '100%'}}>
-                {items.map(({key, isExausted, isDamaged}, index) => isBattleTrack(index) ? null : (
+                {items.map(({key, isExhausted, isDamaged}, index) => isBattleTrack(index) ? null : (
                     <div  key={key} style={{position: "relative", userSelect: 'none', display: 'flex', width: '2rem'}} >
                         <img style={{opacity: '100%', borderRadius: '15%'}} src={Item} width="100%" alt="sawmill" />
-                        {isExausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
+                        {isExhausted && (<div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', backgroundColor: 'black', opacity: '40%', borderRadius: '15%', position: "absolute", top: 0,}}></div>)}                          
                         <div style={{width: '100%', height: '100%', display: 'flex', boxSizing: 'border-box', alignItems: "center", justifyContent: 'center', 
                             fontSize: '1.25rem',
                             wordBreak: 'break-all',
